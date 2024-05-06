6d98c8b7eaf809d1a05c97a8cd6e1da6a20366d8
Yuliia <kotyuliya24@gmail.com>
2024-05-05 20:51:35 -0400

    make build

---
+++ index.a536b75d.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table"),l=document.querySelectorAll("tr"),r=0;r<l.length;r++){var o=e.rows[r].cells[1].cloneNode(!0),c=e.rows[r].cells.length-1;e.rows[r].cells[c].before(o)}
+//# sourceMappingURL=index.a536b75d.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const table = document.querySelector("table");
+const rows = document.querySelectorAll("tr");
+for(let i = 0; i < rows.length; i++){
+    const clonedColumn = table.rows[i].cells[1].cloneNode(true);
+    const lastElementIndex = table.rows[i].cells.length - 1;
+    table.rows[i].cells[lastElementIndex].before(clonedColumn);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
